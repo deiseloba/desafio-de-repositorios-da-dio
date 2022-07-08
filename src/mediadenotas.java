036a2f2aa6e32076db07fa5dc4ea7c510d19bbf3
deiseloba <lobadeyse@gmail.com>
2022-07-07 23:15:07 -0300

    programa que calcula a média de notas de alunos.

---
+++ src/mediadenotas.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class mediadenotas {
+    public static void main(String[] args) {
+       // Todo Auto - generatede method stub
+        double nota1, nota2, nota3,nota4, media;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a nota 1 :  ");
+        nota1 = entrada.nextDouble();
+
+        System.out.println("Digite a nota 2 : ");
+        nota2 = entrada.nextDouble();
+
+        System.out.println("Digite a nota 3  : ");
+        nota3= entrada.nextDouble();
+
+        System.out.println("Digite a nota 4  : ");
+        nota4= entrada.nextDouble();
+
+        media= (nota1+nota2+nota3+nota4)/4;
+        System.out.println("A média do aluno é : "+media);
+
+        if (media >70) {
+            System.out.println("Aluno aprovado");   }
+
+        else if (media>50) {
+            System.out.println("Aluno em recuperação");   }
+        else {System.out.println("Aluno reprovado"); }
+
+
+
+
+
+    }
+}
